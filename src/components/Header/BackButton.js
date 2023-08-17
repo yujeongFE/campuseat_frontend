5f8e61465336b9ab53f4cb62b241e8dff5acb49b
yujeongFE <yujeong6231@naver.com>
2023-08-17 22:16:11 +0900

    [style] 헤더 아이콘 추가

---
+++ src/components/Header/BackButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as BackButtonSvg } from "../../assets/backbutton.svg";
+
+const BackButton = () => {
+  return (
+    <div>
+      <BackButtonSvg />
+    </div>
+  );
+};
+
+export default BackButton;
---
+++ src/components/Header/SidebarButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as SidebarButtonSvg } from "../../assets/sidebutton.svg";
+
+const SidebarButton = () => {
+  return (
+    <div>
+      <SidebarButtonSvg />
+    </div>
+  );
+};
+
+export default SidebarButton;
