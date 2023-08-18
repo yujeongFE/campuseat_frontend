da79b4399f3f39bfb2f27283a716d9d389b52c1b
mirae0831@o365.hufs.ac.kr <mirae0831@o365.hufs.ac.kr>
2023-08-18 23:47:36 +0900

    [add] 랜딩페이지 애니메이션 추가

--- src/pages/Auth/Lending.js
+++ src/pages/Auth/Lending.js
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Lending.css";
+import logoImage from "../../assets/image/Login/hamburger.svg"
+import { keyframes } from "styled-components";
 
 const background = {
   background: 'linear-gradient(180deg, #FF6D1A 0%, #FAB55F 70%, #F4DE9A 100%)',
@@ -20,19 +23,23 @@ const logoWrapper = {
 }
 
 
+const logoBounce = {
+  animation: "bounce 0.4s infinite",
+}; //애니메이션
+
 
 const Lending = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     setTimeout(() => {
-      navigate('/review/create');
-    }, 2000); 
+      navigate('/login');
+    }, 2000);
   }, [navigate]);
   return (
     <div style={background}>
       <div style={logoWrapper}>
-        <img width={180} alt="뭐가 궁금해" src="/images/landing-logo.png" />
+        <img width={180} alt="뭐가 궁금해" src={logoImage} style={logoBounce}/>
         <img width={300} alt="뭐가 궁금해" src="/images/landing-title.png" />
       </div>
     </div>
