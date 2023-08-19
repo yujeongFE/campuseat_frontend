abd45796f0680c319d1d8382b713e2d542fe96c1
yujeongFE <yujeong6231@naver.com>
2023-08-20 01:43:42 +0900

    [repactor] Map 컴포넌트 이름 변경

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Lending from "../pages/Auth/Lending";
 import Login from "../pages/Auth/Login";
 import Main from "../pages/Main/Main";
-import Map from "../pages/Map/Map";
+import MapScreen from "../pages/Map/Map";
 import Shop from "../pages/Shop/Shop";
 import Ranking from "../pages/Shop/Ranking";
 import Review from "../pages/Shop/Review";
@@ -15,9 +15,9 @@ const DefaultRouter = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Lending />} />
-        <Route path="/login" element={<Login />}/>
+        <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
-        <Route path="/map" element={<Map />} />
+        <Route path="/map" element={<MapScreen />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/ranking" element={<Ranking />} />
         <Route path="/review" element={<Review />} />
