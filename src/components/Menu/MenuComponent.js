848d9856562c3e6dc1c03296efad36937934d1ad
mirae0831@o365.hufs.ac.kr <mirae0831@o365.hufs.ac.kr>
2023-08-20 02:38:47 +0900

    [chore] 상점 메뉴 컴포넌트 구현 완료

--- src/components/Menu/MenuComponent.js
+++ src/components/Menu/MenuComponent.js
@@ -5,18 +5,18 @@ import Deungsim from "./images/Deungsim.svg";
 import Curry from "./images/Curry.svg";
 import Cheese from "./images/Cheese.svg";
 import Gochidon from "./images/Gochidon.svg";
-import Dupbab from "./images/Dupbab.svg";
+import Soba from "./images/Soba.svg";
+import Udon from "./images/Udon.svg";
 
 const MenuComponent = () => {
   const [menuItems, setMenuItems] = useState([
-    { id: 1, image:Ansim, name: "안심 카츠", price: 10000 },
-    { id: 2, image:Deungsim, name: "등심 카츠", price: 15000 },
-    { id: 3, image:Curry, name: "카레카츠", price: 18000 },
-    { id: 4, image:Cheese, name: "치즈카츠", price: 10000 },
-    { id: 5, image:Gochidon, name: "고구마치즈카츠", price: 10000 },
-    { id: 6, image:Dupbab, name: "가츠동", price: 10000 },
-    { id: 7, image:"../../assets/image/Login/hamburger.svg", name: "모밀 + 등심돈가스", price: 10000 },
-    // ... 다른 메뉴 항목들 추가
+    { id: 1, image:Ansim, name: "안심 카츠", price: 9000 },
+    { id: 2, image:Deungsim, name: "등심 카츠", price: 10000 },
+    { id: 3, image:Curry, name: "카레카츠", price: 11000 },
+    { id: 4, image:Cheese, name: "치즈카츠", price: 11000 },
+    { id: 5, image:Gochidon, name: "고구마치즈카츠", price: 12000 },
+    { id: 6, image:Soba, name: "소바", price: 7000 },
+    { id: 7, image:Udon, name: "우동", price: 7000 },
   ]);
 
   return (
@@ -26,9 +26,9 @@ const MenuComponent = () => {
       {menuItems.map((item) => (
           <li key={item.id} className="menu-item">
             <div className="item-image">
-              <img src={item.image} style={{width:"123px"}} />
+              <img src={item.image} style={{width:"123px", borderRadius:"10px"}} />
             </div>
-            <span className="item-name">{item.name}</span><br/>
+            <span className="item-name" style={{fontSize:"15px"}}>{item.name}</span><br/>
             <span className="item-price">{item.price}원</span>
           </li>
         ))}
